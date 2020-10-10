32e5656bbba5ecdc8326642e13a9f7607f96937d
ZenithNUC <lizengzhi1998@foxmail.com>
2020-10-10 18:05:10 +0800

    “add demo code of map

--- 5/5-1/5-1.go
+++ 5/5-1/5-1.go
@@ -4,10 +4,23 @@ import (
 	"fmt"
 )
 
-func main() {
+func func1() {
 	var personSalary map[string]int
 	if personSalary == nil {
 		fmt.Println("map is nil. Going to make one.")
 		personSalary = make(map[string]int)
 	}
+}
+
+func func2() {
+	personSalary := make(map[string]int)
+	personSalary["steve"] = 12000
+	personSalary["jamie"] = 15000
+	personSalary["mike"] = 9000
+	fmt.Println("personSalary map contents:", personSalary)
+}
+
+func main() {
+	func1()
+	func2()
 }
