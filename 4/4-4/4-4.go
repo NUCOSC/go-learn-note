d021e20382cce6dc672f427b05e308d0ddf95716
ZenithNUC <lizengzhi1998@foxmail.com>
2020-10-10 18:28:29 +0800

    “change demo code of map

---
+++ 4/4-4/4-4.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+)
+
+func func1() {
+	var personSalary map[string]int
+	if personSalary == nil {
+		fmt.Println("map is nil. Going to make one.")
+		personSalary = make(map[string]int)
+	}
+}
+
+func func2() {
+	personSalary := make(map[string]int)
+	personSalary["steve"] = 12000
+	personSalary["jamie"] = 15000
+	personSalary["mike"] = 9000
+	fmt.Println("personSalary map contents:", personSalary)
+}
+
+func func3() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("personSalary map contents:", personSalary)
+}
+
+func func4() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	employee := "jamie"
+	fmt.Println("Salary of", employee, "is", personSalary[employee])
+}
+
+func func5() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	employee := "jamie"
+	fmt.Println("Salary of", employee, "is", personSalary[employee])
+	fmt.Println("Salary of joe is", personSalary["joe"])
+}
+
+func func6() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	newEmp := "joe"
+	value, ok := personSalary[newEmp]
+	if ok == true {
+		fmt.Println("Salary of", newEmp, "is", value)
+	} else {
+		fmt.Println(newEmp, "not found")
+	}
+}
+
+func func7() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("All items of a map")
+	for key, value := range personSalary {
+		fmt.Printf("personSalary[%s] = %d\n", key, value)
+	}
+}
+
+func func8() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("map before deletion", personSalary)
+	delete(personSalary, "steve")
+	fmt.Println("map after deletion", personSalary)
+}
+
+func func9() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("length is", len(personSalary))
+}
+
+func func10() {
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("Original person salary", personSalary)
+	newPersonSalary := personSalary
+	newPersonSalary["mike"] = 18000
+	fmt.Println("Person salary changed", personSalary)
+}
+
+func main() {
+	func1()
+}
